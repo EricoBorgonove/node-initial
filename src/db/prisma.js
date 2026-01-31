9ddf4a6b3e6679b932c999fa3a819beee819c75b
Erico Borgonove Cruz <ericoborgonovecruz@Ericos-MacBook-Air.local>
2026-01-31 16:35:28 -0400

    prisma.js

---
+++ src/db/prisma.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import pkg from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const { PrismaClient } = pkg;
+
+if (!process.env.DATABASE_URL){
+    throw new Error ("DATABASE_URL não definida no .env")
+}
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+});
+
+export const prisma = new PrismaClient({ adapter });

26799ca3b53bc905b5b294de7e7e3a3cffe90dc0
Erico Borgonove Cruz <ericoborgonovecruz@Ericos-MacBook-Air.local>
2026-01-31 16:41:00 -0400

    prisma.js FINAL

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { prisma } from "./db/prisma.js";
 
 const app = express();
 dotenv.config();
@@ -22,4 +23,13 @@ app.get("/teste", (req, res) =>{
 // ultima coisa do arquivo por enquanto
 app.listen (port, ()=> {
     console.log (`Server rodando em http://localhost:${port}`)
+});
+
+process.on ("SIGINT", async ()=>{
+    await prisma.$disconnect();
+    process.exit(0);
+});
+process.on ("SIGTERM", async ()=>{
+    await prisma.$disconnect();
+    process.exit(0);
 });

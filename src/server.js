b7c141f450de681710d84657523a6a332fc191df
ericoborgonove <borgonove@gmail.com>
2026-02-21 15:54:53 -0400

    feat: rota criar uauários

--- src/server.js
+++ src/server.js
@@ -11,7 +11,27 @@ app.use(express.json());
 app.use(healthRoutes);
 
 const port = Number(process.env.PORT);
+// criar usuarios
 
+app.post("/users", async (req, res)=>{
+    try{
+        const {name, email} = req.body;
+
+
+        if (!name || !email){
+            return res.status(400).json({error: "name e email são obrigatórios"});
+        }
+        const user = await prisma.user.create({
+            data: {name, email},
+        });
+
+        return res.status(201).json(user);
+    }catch (err){
+        return res.status(400).json({error: "não foi possível criar o usuário", 
+            details: String(err.message || err)});
+    }
+
+})
 
 // ultima coisa do arquivo por enquanto
 app.listen (port, ()=> {
